5506bebec645bc471254c4537acf85765e7b552a
Konstantin <kostil2000@yandex.ru>
2022-11-04 18:51:57 +0300

    sem7

--- seminar#1/Task#1.py
+++ seminar#1/Task#1.py
@@ -1,12 +1,73 @@
- #Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-#Пример:
-#- 6 -> да
-#- 7 -> да
-#- 1 -> нет
+#numbers = [-4, 5, 6, -7, -3, 7, 3]
+#count = 0
+# for i in numbers:
+#     if i > 0:
+#         print(i)
 
-num = int(input('Введите число: '))
+# lst = ["dqas", "aadfj", "Aslkd", "jhfjh"]
+#
+# for i in lst:
+#     if i.lower()[0] == "a":
+#         print("ok", i)
+#     else:
+#         print("no", i)
 
-if num == 6 or 7 == num:
-    print('это выходной день!')
-else:
-    print('это не выходной день!')
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# for i in numbers:
+#     if i > 5 and i % 2 == 0:
+#         print(i)
+
+
+# lstB = ["fuck", "dumm"]
+#
+# lstA = input("Введите текст ").split()
+#
+# for i in lstA:
+#     if i in lstB:
+#         print("В тексте содержится мат")
+#         break
+#
+# else:
+#     print("В тексте нет мата")
+
+# numbers = [4.225, 5.5746563, 78.1763754, 99.1176372]
+# for i in numbers:
+#     print(round(i, 3))
+
+
+#
+# for i in range(10, 101, 10):
+#     print(i // 10 )
+
+# lst = [4, 8, 5, 79, 7.55]
+# print(lst[3::-2])
+
+#lst = input("Введите текст ").lower()
+#if lst == lst[::-1]:
+#    print("polydrom")
+#else:
+#    print("no polydrom")
+
+# k = 6
+# a = "Я изучаю {} лет {} питон"
+# print(a.format(k, 9))
+
+
+monat = 1
+
+for i in range(100, -1, -10):      #один проход цикла по телу - итерация
+    print(f"в месяц {monat} останется {i} кг еды".title() + "!") # сложение строк конкатенация
+    monat += 1         #синтаксический сахар
+
+
+
+
+
+
+
+
+
+
+
+
