7202f4aa344d05720c7311d3423ee6432ad3ef73
Konstantin <kostil2000@yandex.ru>
2022-11-06 09:14:52 +0300

    seminar#8

---
+++ seminar#8/main.py
@@ -0,0 +1,8 @@
+# **Задача:** Создать информационную систему позволяющую работать с сотрудниками некой компании \ 
+# студентами вуза \ учениками школы
+
+
+from new_person import writing_down as wd
+import view
+inf_book = wd()
+view.export_book(inf_book)
